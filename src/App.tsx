a0280f712dc812b1fbe4fc1cb64e956ee9b00281
SkjalgS <ssl028@uit.no>
2026-04-03 14:34:30 +0200

    refactor: refactor CSS structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './App.css';
 import { Header } from './components/Layout/Header/Header';
 import { Footer } from './components/Layout/Footer/Footer';
 import Index from './pages/index/Index';
